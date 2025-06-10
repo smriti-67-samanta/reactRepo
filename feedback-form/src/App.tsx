bb2897cfbad27c19525f9deeaa2ae91d96403e5b
smriti-67-samanta <samantasmriti0@gmail.com>
2025-06-10 23:02:46 +0530

    typescript

---
+++ feedback-form/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import FeedbackForm from './components/FeedbackForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Customer Feedback</h1>
+      <FeedbackForm />
+    </div>
+  );
+}
+
+export default App;
